e6ff1d6b419808bdca3eca507e46b320e499fa3d
Dev-Maidu <maidulislammanik8991@gmail.com>
2025-11-21 22:43:35 +0800

    learn how to read async file

--- read-async.js
+++ read-async.js
@@ -1,13 +1,11 @@
-const fs = require("fs");
-
-console.log("start reading...");
-
-fs.readFile("./data/diary.txt", "utf-8", (error, data) => {
-  if (error) {
-    console.error("error happend :", error.message);
+const fs= require("fs");
+console.log("Start Reading File.....");
+fs.readFile("./data/diary.txt","utf-8",(error,data)=>{
+  if(error) {
+    console.error(error.message);
   }
-  console.log("file content :");
-  console.log(data);
-});
+  console.log("File content");
+  console.log(data)
+})
 
 console.log("This runs immediately - no blocking");
--- read-sync.js
+++ read-sync.js
@@ -1,10 +1,12 @@
 const fs= require("fs");
-console.log("start reading.....");
+
+console.log("start reading .....");
+
 try{
     const data=fs.readFileSync("./data/diary.txt","utf-8");
     console.log("file content");
     console.log(data);
 }
 catch(err){
-    console.error(err.message);
+console.error(err.message);
 }
