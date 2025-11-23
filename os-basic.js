695db349c73229cc3804bec883b131da8fc04fc5
Dev-Maidu <maidulislammanik8991@gmail.com>
2025-11-23 21:03:45 +0800

    learn basic os module

---
+++ os-basic.js
@@ -0,0 +1,37 @@
+const os=require("os");
+console.log("System Info\n");
+console.log("-".repeat(60));
+
+console.log("Platform details: ");
+console.log("Platform: ",os.platform());
+console.log("Architecture: ",os.arch());
+console.log("OS type: ",os.type());
+console.log("OS Realise: ",os.release());
+console.log("Hostname: ",os.hostname());
+
+console.log("-".repeat(60));
+console.log("\nCPU info : ");
+const cpus=os.cpus();
+console.log("CPU Model: ",cpus[0].model);
+console.log("Number of core: ",cpus.length);
+console.log("CPU Speed: ",cpus[0].speed)
+
+console.log("-".repeat(60));
+const totalMem = os.totalmem();
+const freeMem = os.freemem();
+console.log(
+  "Total Memory : ",
+  (totalMem / 1024 / 1024 / 1024).toFixed(2),
+  "GB"
+);
+console.log("Free Memory : ", (freeMem / 1024 / 1024 / 1024).toFixed(2), "GB");
+
+console.log("-".repeat(50));
+
+const uptime = os.uptime();
+
+const days = Math.floor(uptime / 86400);
+const hours = Math.floor((uptime % 86400) / 3600);
+const minutes = Math.floor((uptime % 3600) / 60);
+
+console.log(`${days} days ${hours} hours ${minutes} minutes`);
